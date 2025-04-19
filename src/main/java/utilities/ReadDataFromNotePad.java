ea7eb4c2fa935110441c00ca0584a40fc9f4403d
ankush nagpure <nagpureankush1991@gmail.com>
2025-04-19 19:20:33 +0530

    datafile updated

--- src/main/java/utilities/ReadDataFromNotePad.java
+++ src/main/java/utilities/ReadDataFromNotePad.java
@@ -5,6 +5,8 @@ import java.util.Properties;
 
 public class ReadDataFromNotePad {
     Properties prop;
+
+    //function for fetching data from properties file
     public  ReadDataFromNotePad() throws IOException {
         File file = new File("src/main/resources/data.properties");
         FileInputStream fr = new FileInputStream(file);
